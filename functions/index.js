a88ead32754c8ede1dd0055db9ee24772a40315f
Alan Leverenz <awleverenz@aol.com>
2021-08-19 17:13:18 -0400

    created callable function

--- functions/index.js
+++ functions/index.js
@@ -5,10 +5,17 @@ const functions = require("firebase-functions");
 
 exports.randomNumber = functions.https.onRequest((request, response) => {
   const number = Math.round(Math.random() * 100);
+  // can log to the firebase functions log
+  console.log(number);
   response.send(number.toString());
 });
 
 // http request 2
 exports.toTheDojo = functions.https.onRequest((request, response) => {
   response.redirect("https://www.thenetninja.co.uk");
+});
+
+// http callable function
+exports.sayHello = functions.https.onCall((data, context) => {
+  return "hello, ninjas";
 });
