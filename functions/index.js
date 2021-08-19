ada9b4c7827eead284e00ae3966c75b09ed674e2
Alan Leverenz <awleverenz@aol.com>
2021-08-19 16:53:44 -0400

    added 2nd firebase function

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,8 @@ exports.randomNumber = functions.https.onRequest((request, response) => {
   const number = Math.round(Math.random() * 100);
   response.send(number.toString());
 });
+
+// http request 2
+exports.toTheDojo = functions.https.onRequest((request, response) => {
+  response.redirect("https://www.thenetninja.co.uk");
+});
